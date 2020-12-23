3e927349d6bb7dce36707f8984ca9144469e9ba3
Saiba <swapniksaiba@gmail.com>
2020-12-23 17:09:57 +0530

    venv created

--- virtual/Scripts/activate.bat
+++ virtual/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=T:\Nov\Python-Series\04-Website-Builder-with-Flask\virtual
+set VIRTUAL_ENV=T:\Nov\Python-Series\04-Website-Builder-with-Flask\env\virtual
 
 if not defined PROMPT set PROMPT=$P$G
 
